1d9b418e8e67448d876a65b4290d2e22d964b153
prodiksun <47965173+prodiksun@users.noreply.github.com>
2022-07-15 15:27:29 +0300

    Add result

--- GetStatusUrl.sh
+++ GetStatusUrl.sh
@@ -1,1 +1,14 @@
-curl -I https://habr.ru 2>/dev/null | head -n 1 | cut -d$' ' -f2
+#!/bin/bash
+
+URL="https://ya.ru"
+
+result=`curl -I $URL 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+
+if [[ $result -eq 200 ]]
+then
+  result="success"
+else
+  result="failure"
+fi
+
+echo ${result}
