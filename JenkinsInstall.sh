e4af8dea24f71aba66590af9231a16edf7d89086
prodiksun <47965173+prodiksun@users.noreply.github.com>
2022-07-15 14:13:07 +0300

    Remove lines

--- JenkinsInstall.sh
+++ JenkinsInstall.sh
@@ -1,11 +1,13 @@
+# Update & Upgrade
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y autoremove
+# Install Java
+sudo apt-get install openjdk-11-jdk
+# Install Jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo apt install -y jenkins
 sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword 
